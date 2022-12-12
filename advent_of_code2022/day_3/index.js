194ae253bea20f55213a1cf9f241a6ba7de43d19
Kaleb Lyda <kaleblyda14@gmail.com>
2022-12-12 18:51:17 +0000

    day 3 part 2

--- advent_of_code2022/day_3/index.js
+++ advent_of_code2022/day_3/index.js
@@ -2,14 +2,20 @@ var readline = require('readline');
 var fs = require('fs')
 
 let matches = ''
-
+let group = []
 readline.createInterface({
-    input: fs.createReadStream('test-data.txt'),
+    input: fs.createReadStream('data.txt'),
     terminal: false
 }).on('line', function(line) {
-    let l = line.slice(0, line.length / 2)
-    let r = line.slice(line.length / 2)
-    matches += findMatches(l, r)
+    // let l = line.slice(0, line.length / 2)
+    // let r = line.slice(line.length / 2)
+    // matches += findMatches(l, r)
+    group.push(line)
+    if (group.length === 3) {
+        matches += findBadgeInSet(group)
+        group = []
+    }
+
 }).on('close', function() {
     console.log('TOTAL', totalMatchPriorities(matches))
 })
@@ -46,7 +52,7 @@ function totalMatchPriorities(matches) {
     console.log('MATCHES', matches)
     let total = 0
     for (let i = 0; i < matches.length; i++) {
-        console.log('CHAR CODE AT: ', matches[i], matches.charCodeAt(i))
+        // console.log('CHAR CODE AT: ', matches[i], matches.charCodeAt(i))
         let matchCharCode = matches.charCodeAt(i) // html code
         if (matchCharCode <= 90) { // is upper case
             total += matchCharCode - 38
@@ -55,4 +61,34 @@ function totalMatchPriorities(matches) {
         }
     }
     return total
+}
+
+function findBadgeInSet(arr) {
+    //in: set of 3 strings (group's sacks)
+    // out: char equalling that group's badge (common to all 3 strings)
+    // maybe: one object, count to 3?
+    let sets = [{}, {}, {}]
+    for (let i = 0; i < arr.length; i++) {
+        for (let char of arr[i]) {
+            if (sets[i][char]) {
+                sets[i][char]++
+            } else {
+                sets[i][char] = 1
+            }
+        }
+    }
+    let badge = ''
+    let keys = Object.keys(sets[0])
+    console.log(sets[0]['r'])
+    console.log(sets[1]['r'])
+    console.log(sets[2]['r'])
+    for (let i = 0; i < keys.length; i++) {
+        let letter = keys[i]
+        if (sets[0][letter] && sets[1][letter] && sets[2][letter]) {
+            console.log('ALL 3 ', letter)
+            badge += letter
+        }
+    }
+    console.log('BADGE: ', badge)
+    return badge
 }
